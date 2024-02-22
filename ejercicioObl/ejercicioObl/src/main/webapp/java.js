2267b68a097338b1c9e35ff76e066e251752890b
DavidMunoz18 <145343117+DavidMunoz18@users.noreply.github.com>
2024-02-22 20:04:43 +0100

    Add files via upload

---
+++ ejercicioObl/ejercicioObl/src/main/webapp/java.js
@@ -0,0 +1,35 @@
+document.write("CASO 1"); 
+
+
+var a = 5;
+var b = 10;
+
+if (a === 5) {
+  let a = 4;
+  var b = 1;
+
+  
+
+document.write(a);
+  
+
+document.write(b);
+}
+
+document.write(a);
+document.write(b);
+
+document.write("CASO 2");
+for (let i = 0; i<10; i++) {
+  
+
+document.write(i);
+}
+
+document.write(i);
+
+document.write("CASO 3");
+    var z = 5
+    let x = z+10, y = 12;
+   
+    document.write(x + y);
